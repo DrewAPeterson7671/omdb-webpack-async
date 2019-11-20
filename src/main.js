f87ec05b8a6a495a21d831b5ee7cc4b63c120e7a
Student <example@example.com>
2019-11-20 11:33:22 -0800

    using async to fetch omdb api

--- src/main.js
+++ src/main.js
@@ -8,19 +8,17 @@ $(document).ready(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    fetch(`http://www.omdbapi.com/?t=${city}&apikey=${process.env.API_KEY}`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(jsonifiedResponse) {
-        getElements(jsonifiedResponse);
-      });
+    asyncApiCall();
 
-   const getElements = function(response) {
+    async function asyncApiCall() {
+      let response = await fetch(`http://www.omdbapi.com/?t=${city}&apikey=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      getElements(jsonifiedResponse);
+    }
+
+    const getElements = function(response) {
       $('.showHumidity').text(`${response.Title}`);
        $('.showTemp').text(`${response.Director}`);
-      // $('.showHumidity').text(`The humidity in ${city} is ${response.title}%`);
-      // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
     }
   });
 });
